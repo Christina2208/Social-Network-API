16564aa5f97e6b1afb87a9d2dd07e41f0f9e3925
Christina2208 <christinalf22@gmail.com>
2023-09-11 13:48:55 -0400

    Importing/exporting the models for useage

---
+++ models/index.js
@@ -0,0 +1,7 @@
+// Importing all the models
+const User = require("./User");
+const Thought = require("./Thought");
+const Reaction = require("./Reaction");
+
+// Exporting the models
+module.exports = { User, Thought, Reaction };
