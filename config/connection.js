bd4e545f9524b349bb87471eabde58923a19f574
Christina2208 <christinalf22@gmail.com>
2023-09-11 13:21:06 -0400

    created the connection for the social network db

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+// Importing the mongoose library
+const mongoose = require('mongoose');
+
+// Connect to a MongoDB database named 'social-network' running on localhost at port 27017
+mongoose.connect('mongodb://localhost:27017/social-network', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+//Exporting the mongoose connection
+module.exports = mongoose.connection;
